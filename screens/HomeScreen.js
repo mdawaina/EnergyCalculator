965eb6d29111249f4df88aaff617fe4d8169f40f
mdawaina <mohameddawaina@gmail.com>
2019-02-08 07:07:26 +0300

    Cleared home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -25,7 +25,7 @@ export default class HomeScreen extends React.Component {
             <Glocation />
           </View>
 
-          <View style={styles.getStartedContainer}>
+        {/*   <View style={styles.getStartedContainer}>
             {this._maybeRenderDevelopmentModeWarning()}
 
             <Text style={styles.getStartedText}>Get started by opening</Text>
@@ -37,22 +37,12 @@ export default class HomeScreen extends React.Component {
             <Text style={styles.getStartedText}>
               Change this text and your app will automatically reload.
             </Text>
-          </View>
+          </View> */}
 
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>
+         
         </ScrollView>
 
-        <View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>
+       
       </View>
     );
   }
